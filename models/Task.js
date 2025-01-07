7d58565f7d4d85b55287f51f4fd32801d77aa050
abdelhakim benbouanane <88632705+mrbenboyy@users.noreply.github.com>
2025-01-07 02:11:33 +0100

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
-    dueDate: { type: Date },
+    dueDate: { type: Date, default: Date.now  },
     priority: { type: String, enum: ['Low', 'Medium', 'High'], default: 'Medium' },
     status: { type: String, enum: ['Pending', 'Ongoing', 'Completed'], default: 'Pending' },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
